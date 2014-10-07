f80ba00c79f34f01f7b3a818277aeaa049e77176
Wojciech Wojtyniak <wojciech@wojtyniak.com>
2014-10-06 22:51:36 -0700

    Adding geiser and racket

--- emacs.d/config/lisp-config.el
+++ emacs.d/config/lisp-config.el
@@ -29,3 +29,8 @@
 (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'geiser-repl-mode-hook      #'enable-paredit-mode)
+
+
+;;;;;;;; RACKET ;;;;;;;;
+(setq geiser-racket-binary "/Applications/Racket v6.1/bin/racket")
