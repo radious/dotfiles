909bb7ca2b88ae8b33ae202e1839ff3216ba5acf
Wojciech Wojtyniak <wojciech@wojtyniak.com>
2015-08-30 00:14:11 -0700

    Moving font settings to the correct file

--- emacs.d/config/darwin.el
+++ emacs.d/config/darwin.el
@@ -3,4 +3,14 @@
 (when (eq system-type 'darwin)
   (setq mac-command-modifier 'meta) ; right cmd is now meta
   (setq mac-right-option-modifier nil) ; right alt is right alt, no modifiers
-  (set-face-attribute 'default nil :height 140))
+
+  ;;;;;;;; FONT ;;;;;;;;
+  ;; default Latin font (e.g. Consolas)
+  (set-face-attribute 'default nil :family "Menlo")
+
+  ;; default font size (point * 10)
+  ;;
+  ;; WARNING!  Depending on the default font,
+  ;; if the size is not supported very well, the frame will be clipped
+  ;; so that the beginning of the buffer may not be visible correctly.
+  (set-face-attribute 'default nil :height 120))
--- emacs.d/config/editing.el
+++ emacs.d/config/editing.el
@@ -3,19 +3,6 @@
 (setq-default tab-width 2)              ;; short tabs, monitors are small
 ;; (setq indent-line-function 'insert-tab) ;; insert tab when tab key is hit
 
-;;;;;;;; FONT ;;;;;;;;
-(when (eq system-type 'darwin)
-
-  ;; default Latin font (e.g. Consolas)
-  (set-face-attribute 'default nil :family "Menlo")
-
-  ;; default font size (point * 10)
-  ;;
-  ;; WARNING!  Depending on the default font,
-  ;; if the size is not supported very well, the frame will be clipped
-  ;; so that the beginning of the buffer may not be visible correctly.
-  (set-face-attribute 'default nil :height 120))
-
 ;;;;;;;; INDENTING ;;;;;;;;
 (define-key global-map (kbd "C-j") 'newline)
 
