4418c955ca593d0024641edfdf75695bc74ae898
Wojciech Wojtyniak <wojciech@wojtyniak.com>
2015-11-29 22:52:52 -0800

    tool-bar-mode only on window systems

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -24,6 +24,7 @@
     (load-library "local"))
 
 ;; varia
-(tool-bar-mode -1) ; don't want toolbar on my display
+(if window-system
+        (tool-bar-mode 0)) ; don't want toolbar on my display
 (column-number-mode) ; like to see columns
 (show-paren-mode 1) ; match my parens!
